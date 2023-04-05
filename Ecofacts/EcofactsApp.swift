ef6d34646a1522592d6e65e8c87cae8320664504
Romi <romi.peli@pm.me>
2023-04-05 15:44:41 +0200

    Initial Commit

---
+++ Ecofacts/EcofactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcofactsApp.swift
+//  Ecofacts
+//
+//  Created by Bingo Zohar on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcofactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
